f9561691cd89c14027ee9207eeca3effd4f9bb54
Armando Barragan <armandobp765@gmail.com>
2023-11-23 12:08:16 -0600

    Nodemon added to development environment

---
+++ config/settings.js
@@ -0,0 +1,19 @@
+const mysql = require('mysql2');
+
+
+const SECRET_KEY = process.env.SECRET_KEY;
+const PORT = process.env.PORT;
+
+
+const pool = mysql.createPool({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
+    waitForConnections: process.env.WAIT_FOR_CONNECTIONS,
+    connectionLimit: process.env.CONNECTION_LIMIT,
+    queueLimit: process.env.QUEUE_LIMIT
+});
+
+exports.dbPool = pool.promise();
+exports.PORT = PORT;
--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const { PORT } = require("./config/settings")
+const companyRouter = require("./app/views/company")
+
+
 const app = express();
-const port = 3000;
+app.use("", companyRouter)
 
 
-app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
 });

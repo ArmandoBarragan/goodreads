6bd9cfab29d06b0fa7f690920141b2e31478d004
Armando Barragan <armandobp765@gmail.com>
2023-11-23 12:49:30 -0600

    Added stored procedures to schema.sql

--- app/views/company.js
+++ app/views/company.js
@@ -6,33 +6,7 @@ const router = express.Router();
 
 
 router.get('/', (req, res) => {
-    dbPool.getConnection((err, connection) => {
-        // Use the connection for queries
-        connection.query('SELECT 1 + 1 AS result', (queryErr, results) => {
-            // Release the connection back to the `dbPool`
-            connection.release();
-            
-            if (queryErr) {
-              console.error('Error executing query:', queryErr);
-            }
-        
-            console.log('Query result:', results);
-        
-            // Close the dbPool when done (optional)
-            dbPool.end((endErr) => {
-              if (endErr) {
-                console.error('Error closing dbPool:', endErr);
-              }
-        
-              console.log('Connection dbPool closed.');
-            });
-          });
-        if (err) {
-          console.error('Error getting connection from dbPool:', err);
-        }
-    });
     res.send('¡Hola, esta es la página de inicio!');
-
 });
 
 router.get('/about', (req, res) => {
--- compose/db/schema.sql
+++ compose/db/schema.sql
@@ -20,3 +20,113 @@ CREATE TABLE IF NOT EXISTS book (
     author_id INT,
     FOREIGN KEY (author_id) REFERENCES author(author_id)
 );
+
+-- Due to lack of time, I developed every stored procedure in this file. For a more professional
+-- environment, I would use an entire folder that stores every procedure within its own sepparate
+-- file
+
+-- Create User
+CREATE PROCEDURE createUser(
+    IN p_username VARCHAR(255),
+    IN p_email VARCHAR(255)
+)
+BEGIN
+    INSERT INTO user (username, email) VALUES (p_username, p_email);
+END //
+
+DELIMITER ;
+
+-- Delete User
+CREATE PROCEDURE deleteUser(
+    IN p_user_id INT
+)
+BEGIN
+    DELETE FROM user WHERE user_id = p_user_id;
+END //
+
+DELIMITER ;
+
+-- Search Users
+CREATE PROCEDURE searchUsers(
+    IN keyword VARCHAR(255)
+)
+BEGIN
+    SELECT * FROM user
+    WHERE username LIKE CONCAT('%', keyword, '%') OR
+          email LIKE CONCAT('%', keyword, '%');
+END //
+
+DELIMITER ;
+
+-- Create Book
+CREATE PROCEDURE createBook(
+    IN p_title VARCHAR(255),
+    IN p_author_id INT,
+    IN p_published_year INT
+)
+BEGIN
+    INSERT INTO book (title, author_id, published_year)
+    VALUES (p_title, p_author_id, p_published_year);
+END //
+
+DELIMITER ;
+
+-- Delete Book
+DELIMITER //
+
+CREATE PROCEDURE deleteBook(
+    IN p_book_id INT
+)
+BEGIN
+    DELETE FROM book WHERE book_id = p_book_id;
+END //
+
+DELIMITER ;
+
+-- Search Books
+DELIMITER //
+
+CREATE PROCEDURE searchBooks(
+    IN keyword VARCHAR(255)
+)
+BEGIN
+    SELECT * FROM book b
+    JOIN author a ON b.author_id = a.author_id
+    WHERE b.title LIKE CONCAT('%', keyword, '%') OR
+          a.author_name LIKE CONCAT('%', keyword, '%');
+END //
+
+DELIMITER ;
+
+-- Create Author
+CREATE PROCEDURE createAuthor(
+    IN p_author_name VARCHAR(255)
+)
+BEGIN
+    INSERT INTO author (author_name) VALUES (p_author_name);
+END //
+
+DELIMITER ;
+
+-- Delete Author
+CREATE PROCEDURE deleteAuthor(
+    IN p_author_id INT
+)
+BEGIN
+    DELETE FROM author WHERE author_id = p_author_id;
+END //
+
+DELIMITER ;
+
+-- Search Author
+DELIMITER //
+
+CREATE PROCEDURE searchAuthors(
+    IN keyword VARCHAR(255)
+)
+BEGIN
+    SELECT * FROM author
+    WHERE author_name LIKE CONCAT('%', keyword, '%');
+END //
+
+DELIMITER ;
